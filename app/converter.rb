c410c35d8c9a2cad1a0ad4302582f123292ce546
user <coolmurthy@gmail.com>
2015-10-16 16:24:01 +0530

    Roman to Arabic

--- app/converter.rb
+++ app/converter.rb
@@ -2,7 +2,13 @@ class Converter
   def initialize
     @units_dictionary = {0 => "",1 => "I", 2 => "II", 3 => "III", 4 => "IV", 5 => "V", 6 => "VI",7 => "VII",8 => "VIII",9 => "IX"}
     @tens_dictionary = {1 => "X", 2 => "XX", 3 => "XXX", 4 => "XL", 5 => "L", 6 => "LX",7 => "LXX",8 => "LXXX",9 => "XC"}
+    @roman_array = [ "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX","X" ]
   end
+
+  def convert_to_numeral(roman_numeral)  
+    raise "Not a Number ! Only Integers Allowed" unless (roman_numeral.is_a?(String))
+    @roman_array.index(roman_numeral) + 1  
+  end 
   def convert_to_roman(arabic_numeral)
     raise "Not a Number ! Only Integers Allowed" unless ((arabic_numeral.is_a?(Integer)) && (arabic_numeral > 0))
     if arabic_numeral < 10
@@ -12,7 +18,6 @@ class Converter
     end
   end
   private 
-
   def convert_for_2digts(arabic_numeral)
     
     if (arabic_numeral < 100)
@@ -24,7 +29,7 @@ class Converter
     else
       puts "Invalid Input"
     end
-  end   
+  end 
 end
 
 
--- spec/app/converter_spec.rb
+++ spec/app/converter_spec.rb
@@ -18,6 +18,28 @@ describe Converter do
     it 'should return "L" when 50 is entered' do
       Converter.new.convert_to_roman(50).should eq "L"
     end
+    it 'should return "LI" when 51 is entered' do
+      Converter.new.convert_to_roman(51).should eq "LI"
+    end
+     it 'should return "XCIX" when 99 is entered' do
+      Converter.new.convert_to_roman(99).should eq "XCIX"
+    end
+  end 
+
+  context "Roman to Arabic" do 
+    it 'should only acccept Strings' do 
+      expect {Converter.new.convert_to_numeral(4)}.to raise_error
+    end 
+
+    it 'should return 1 when "I" is entered ' do
+      Converter.new.convert_to_numeral("I").should eq 1
+    end
+     it 'should return 3 when "III" is entered ' do
+      Converter.new.convert_to_numeral("III").should eq 3
+    end
+     it 'should return 10 when "X" is entered ' do
+      Converter.new.convert_to_numeral("X").should eq 10
+    end
   end
 end
 
